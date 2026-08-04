c91406ebf7074b106d677c5d3bdb97de4af50c0c
hirowassan <hrkwhrms@gmail.com>
2026-08-05 00:56:14 +0900

    add budoux

--- src/components/common/MarkdownContent.tsx
+++ src/components/common/MarkdownContent.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useMemo, useRef } from 'react'
+import { loadDefaultJapaneseParser } from 'budoux'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
@@ -7,5 +9,20 @@ interface MarkdownContentProps {
 
 /** Renders a Markdown body (frontmatter already stripped) as React elements. */
 export function MarkdownContent({ content }: MarkdownContentProps) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+  const containerRef = useRef<HTMLDivElement>(null)
+  const parser = useMemo(() => loadDefaultJapaneseParser(), [])
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      return
+    }
+
+    parser.applyToElement(containerRef.current)
+  }, [content, parser])
+
+  return (
+    <div ref={containerRef}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    </div>
+  )
 }
